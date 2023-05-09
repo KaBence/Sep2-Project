78a15753e2b90beea3274fcd1ec228c8a9eb1699
Flolol0o <116642272+Flolol0o@users.noreply.github.com>
2023-05-09 16:21:56 +0200

    ListView > Table list (in our case)

--- src/Client/View/Controllers/EmployeeHomeController.java
+++ src/Client/View/Controllers/EmployeeHomeController.java
@@ -8,10 +8,7 @@ import Server.Model.Customer;
 import Server.Model.Employee;
 import Server.Model.Room;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
@@ -25,30 +22,11 @@ public class EmployeeHomeController
   @FXML Tab customerinfo;
   @FXML Tab reservation;
 
-  @FXML TableView<Employee> tableViewEmployee;
-  @FXML TableColumn<Employee, Integer> employeeId;
-  @FXML TableColumn<Employee, String> employeeFirstName;
-  @FXML TableColumn<Employee, String> employeeLastName;
-  @FXML TableColumn<Employee, String> employeePosition;
-  @FXML TableColumn<Employee, String> employeePhoneNumber;
+  @FXML ListView<String> employeeListView;
+  @FXML ListView<String> customerListView;
+  @FXML ListView<Room> roomListView;
 
-  @FXML TableView<Customer> tableViewCustomer;
-  @FXML TableColumn<Customer, String> CustomerEmail;
-  @FXML TableColumn<Customer, String> CustomerFirstName;
-  @FXML TableColumn<Customer, String> CustomerLastName;
-  @FXML TableColumn<Customer, String> CustomerPhoneNumber;
-  @FXML TableColumn<Customer, String> CustomerPayment;
 
-  @FXML TableView<Room> TableViewReservation;
-  @FXML TableColumn<Room, Integer> TableViewRoomNo;
-  @FXML TableColumn<Room, String> TableViewReservationCustomer;
-  @FXML TableColumn<Room, String> TableViewFromDate;
-  @FXML TableColumn<Room, String> TableViewToDate;
-  @FXML TableView<Room> tableView;
-  @FXML TableColumn<Room, Integer> tableRoomNo;
-  @FXML TableColumn<Room, Integer> tableBeds;
-  @FXML TableColumn<Room, Integer> tableSize;
-  @FXML TableColumn<Room, String> tableOri;
   private Region root;
   private ViewHandler viewHandler;
   private EmployeeHomeViewModel viewModel;
@@ -58,22 +36,11 @@ public class EmployeeHomeController
   {
     this.viewHandler = viewHandler;
     this.viewModel = viewModel;
-    this.viewModel.bindRoomList(tableView.itemsProperty());
+    this.viewModel.bindRoomList(roomListView.itemsProperty());
     this.root = root;
 
   }
 
-  public void initialize()
-  {
-    tableOri.setCellValueFactory(
-        new PropertyValueFactory<Room, String>("orientation"));
-    tableBeds.setCellValueFactory(
-        new PropertyValueFactory<Room, Integer>("noOfBeds"));
-    tableSize.setCellValueFactory(
-        new PropertyValueFactory<Room, Integer>("size"));
-    tableRoomNo.setCellValueFactory(
-        new PropertyValueFactory<Room, Integer>("RoomNo"));
-  }
 
   public Region getRoot()
   {
@@ -98,7 +65,7 @@ public class EmployeeHomeController
 
   @FXML void tableClick(MouseEvent event)
   {
-    viewModel.saveRoom(tableView.getSelectionModel().getSelectedItem());
+    viewModel.saveRoom(roomListView.getSelectionModel().getSelectedItem());
     if (event.getClickCount() == 2)
       viewHandler.openView(SceneNames.EditRoom);
   }
