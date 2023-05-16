7b629ee760dfc5b366a0aeb2be1cf7bb8edddd08
Michaela202 <veselovskamichaela222@gmail.com>
2023-05-16 15:11:39 +0200

    reservationdata implementation partialy

--- src/Server/Utility/DataBase/Reservation/ReservationData.java
+++ src/Server/Utility/DataBase/Reservation/ReservationData.java
@@ -10,7 +10,7 @@ public interface ReservationData
   Reservation addNewReservation(int roomNumber, String username, MyDate fromDate, MyDate toDate, boolean CheckedIn);
   ArrayList<Reservation> getMyReservation(String username);
 
-  String editReservation(int roomNumber, String username, MyDate fromDate, MyDate toDate);
+  String updateReservation(int roomNumber, String username, MyDate fromDate, MyDate toDate, boolean CheckedIn);
   ArrayList<Reservation> getAllRooms();
 
 }
