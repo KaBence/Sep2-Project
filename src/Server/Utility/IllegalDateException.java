089fe80a8bbb07aebf110067f376b57b1d5e176d
barbyabr <116642519+barbyabr@users.noreply.github.com>
2023-05-24 21:04:05 +0200

    Dates alerts

--- src/Server/Utility/IllegalDateException.java
+++ src/Server/Utility/IllegalDateException.java
@@ -10,15 +10,16 @@ public class IllegalDateException extends RuntimeException
 
   public String message(){
     return switch (check){
-      case 1-> "the dates are outside a reservation";
-      case 2-> "The dates are the same";
-      case 3-> "Either dates are the same";
-      case 4-> "From date in between and to date is after";
-      case 5-> "Both between";
-      case 6-> "From is before and return in between";
-      case 7-> "From is before and to is equals";
-      case 8 ->"From or to Date is empty";
-      case 9 ->"From date or finish date is before today";
+      case 1-> "In between the dates you want to reserve there is already pending reservation. Please change the dates. ";
+      case 2-> "The dates you have chosen, for the beginning and the and of your stay, are identical. Please change the dates. ";
+      case 3-> "One of the dates that you have chosen is the same as one of the reservation already created. Please change the dates. ";
+      case 4-> "The chosen date for the beginning of your stay is during somebody else's reservation and the chosen day for the end of it is okay. Please change the first date. ";
+      case 5-> "Your whole reservation is during somebody else's stay. Please change the room or the dates. ";
+      case 6-> "The chosen date for the beginning of your stay is during somebody else's reservation. Please change that date. ";
+      case 7-> "The chosen date for the end of your stay needs to be at least a day before. ";
+      case 8 ->"Please, fill in both dates. ";
+      case 9 ->"It is not possible to create reservations in the past. ";
+      case 10 -> "The date for the end of your stay is before the date for the beginning of your stay. ";
       default -> throw new IllegalStateException("Unexpected value: " + check);
     };
   }
